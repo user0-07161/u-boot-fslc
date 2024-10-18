b7a06fb406208ce6c51fea84e2a17fa3fa2d8f08
User0 <104380885+user0-07161@users.noreply.github.com>
2024-10-18 17:39:43 +0200

    Update mx6slpage2.h

--- include/configs/mx6slpage2.h
+++ include/configs/mx6slpage2.h
@@ -2,17 +2,22 @@
 /*
  * Copyright 2021 Andreas Kemnade
  *
- * Configuration settings for the Kobo Nia board.
+ * Configuration settings for the Kobo Clara HD board.
  */
+
 #ifndef __CONFIG_H
 #define __CONFIG_H
+
 #include "mx6_common.h"
+
 /* Size of malloc() pool */
 #define CONFIG_SYS_MALLOC_LEN		(16 * SZ_1M)
+
 #if 0
 #define CONFIG_MXC_UART
 #endif
 #define CONFIG_MXC_UART_BASE		UART1_BASE
+
 /* I2C Configs */
 #ifdef CONFIG_CMD_I2C
 #define CONFIG_SYS_I2C_MXC
@@ -21,6 +26,14 @@
 #define CONFIG_SYS_I2C_MXC_I2C3		/* enable I2C bus 3 */
 #define CONFIG_SYS_I2C_SPEED		  100000
 #endif
+#ifndef CONFIG_SPL_BUILD
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0) \
+	func(MMC, mmc, 1)
+
+#include <config_distro_bootcmd.h>
+#endif
+
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	BOOTENV \
 	"console=ttymxc0\0" \
@@ -31,28 +44,36 @@
 	"scriptaddr=0x87000000\0" \
 	"initrd_high=0xffffffff\0" \
 	"fdt_file=imx6sl-tolino-page2.dtb\0"
+
 /* Physical Memory Map */
 #define PHYS_SDRAM			MMDC0_ARB_BASE_ADDR
 #define PHYS_SDRAM_SIZE			SZ_512M
+
 #define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM
 #define CONFIG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
 #define CONFIG_SYS_INIT_RAM_SIZE	IRAM_SIZE
+
 #define CONFIG_SYS_INIT_SP_OFFSET \
 	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
 #define CONFIG_SYS_INIT_SP_ADDR \
 	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+
 #define CONFIG_MMCROOT			"/dev/mmcblk0p2"  /* USDHC1 */
+
+#define CONFIG_CMDLINE_TAG                      /* enable passing of ATAGs */
+#define CONFIG_SETUP_MEMORY_TAGS
+#define CONFIG_INITRD_TAG
+#define CONFIG_REVISION_TAG
+
 /* MMC Configs */
 #define CONFIG_SYS_FSL_ESDHC_ADDR	USDHC2_BASE_ADDR
 #define CONFIG_SYS_FSL_USDHC_NUM	3
 #define CONFIG_SYS_MMC_ENV_DEV		0  /* USDHC2 */
-#if 0
-#define CONFIG_IMX_THERMAL
-#endif
-#define CONFIG_IOMUX_LPSR
+
 /* USB Configs */
 #ifdef CONFIG_USB
 #define CONFIG_MXC_USB_PORTSC		(PORT_PTS_UTMI | PORT_PTS_PTW)
 #endif
+
 #include <linux/stringify.h>
 #endif				/* __CONFIG_H */
