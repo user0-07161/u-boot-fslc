24e867ec7f7ce186dbff8099e4d03b58eb4a690b
User0 <104380885+user0-07161@users.noreply.github.com>
2024-10-18 17:14:56 +0200

    Update mx6slpage2.c

--- board/kobo/mx6slpage2/mx6slpage2.c
+++ board/kobo/mx6slpage2/mx6slpage2.c
@@ -7,7 +7,7 @@
 #include <asm/arch/clock.h>
 #include <asm/arch/crm_regs.h>
 #include <asm/arch/iomux.h>
-#include <asm/arch/imx-regs.h>
+#include <asm/arch-mx6/imx-regs.h>
 #include <asm/arch/mx6-pins.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/gpio.h>
